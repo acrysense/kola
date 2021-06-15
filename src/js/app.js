a9d6516ac0da93a415ed6557678013ed0d0516d0
acrysense <54480354+acrysense@users.noreply.github.com>
2021-06-15 20:28:15 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // INPUTMASK
+    Inputmask().mask(document.querySelectorAll('input'))
+
+    // HEIGHT 100VH FIX FOR IOS
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    // SLIM SELECT
+    new SlimSelect({
+        select: '#select1'
+    });
+    new SlimSelect({
+        select: '#select2'
+    });
+    
+    // MOBILE MENU
+    const hamburger = document.getElementById('hamburger-toggle')
+    const overlay = document.querySelector('.overlay')
+
+    hamburger.addEventListener('click', () => {
+        if (hamburger.classList.contains('hamburger--active') && overlay.classList.contains('overlay--open')) {
+            hamburger.classList.remove('hamburger--active')
+            overlay.classList.remove('overlay--open')
+            document.body.classList.remove('scroll-disabled')
+        } else {
+            hamburger.classList.add('hamburger--active')
+            overlay.classList.add('overlay--open')
+            document.body.classList.add('scroll-disabled')
+        }
+    });
+});
